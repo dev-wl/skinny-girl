03d95362a0a99e53bd3e5d3cc7c1107861b354fc
wl-ak <wl-ak@wonderslab.com>
2015-10-12 16:19:12 +0300

    Pushing to dev

---
+++ public/wp-content/themes/skinnygirl/page-all-coffees.php
@@ -0,0 +1,90 @@
+<?php get_header(); ?>
+
+<style>
+body.fixed-menu #head-wrapper {
+	display: block;
+}
+
+#menu-wrapper {
+	position: relative;
+	box-shadow: 0 5px 14px 0 rgba(0, 0, 0, 0.5);
+}
+</style>
+
+<script>
+	$(window).on('scroll', function(e) {
+	var st = $(window).scrollTop();
+
+	var hwHeight = $('#head-wrapper').height();
+
+	if (st > hwHeight) {
+		$('body').addClass('fixed-menu');
+		
+	} else {
+		$('body').removeClass('fixed-menu');
+	}
+});
+	$(window).on('load', function() {
+		$('a[href="/products"]').click(function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+		});
+
+	});
+</script>
+	
+	<a name="section2" id="sect2"></a>
+    <div class="head-wrapper products-header" style='height: 304px;background-image: url("/wp-content/themes/skinnygirl/head-wrapper-bg.png");background-repeat: repeat-x;background-position: left top;background-clip: content-box;background-size: 100% 25%;'>
+					<div id="h1" class="head" style='height:310px;background-image: url("/wp-content/themes/skinnygirl/set_2.png");max-width: 1024px;margin: 0px auto;background-position: center top;background-repeat: no-repeat;background-size: 100% auto;'></div>
+				</div>
+
+<div id="blog" style="max-width: 936px;">
+	
+	<p class="desc">
+		<?php 
+		  $cat_desc = get_page_by_title( 'Coffees description', 'ARRAY_A', 'post' );
+		  echo $cat_desc['post_content'];
+		?>
+	</p>
+
+<?php
+                global $post;
+                $args = array('category' => 11, 'numberposts' => -1, 'paged' => ( get_query_var('paged') ? get_query_var('paged') : 1 ) );
+                $myposts = get_posts( $args );
+                $i = 0;
+                foreach ( $myposts as $post ) : {
+                    setup_postdata( $post ); ?>
+					<?php if ($i == 0): ?>
+							<div class="product-row">
+					<?php endif; ?>
+					<?php $i++; ?>
+                    <div class="product">
+                    	<?php if (has_post_thumbnail()): ?>
+                    		<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+							<img src='<?php echo $url; ?>' />
+						<?php endif; ?>
+						<div class="prod-description">
+	                    	<h4><?php the_title(); ?></h4>
+	                        <div class='shortened'><p><?php the_excerpt(); ?></p></div>
+	                        <h5><a href="<?php the_permalink(); ?>">View product</a></h5>
+                            <!--Share This-->
+<span class='st_facebook_large' displayText='Facebook'></span>
+<span class='st_fblike_large' displayText='Facebook Like'></span>
+<span class='st_twitter_large' displayText='Tweet'></span>
+<span class='st_pinterest_large' displayText='Pinterest'></span>
+<span class='st_email_large' displayText='Email'></span>
+<span class="orientation-flag"></span>
+<!--End Share This-->
+                        </div>
+					</div>
+
+					<?php if($i == 2 || end($myposts) == $post) : ?>
+						</div>
+					<?php $i = 0; endif; ?>
+                <?php } endforeach;
+                wp_reset_postdata(); ?>
+</div>
+
+	
+ <?php wp_footer(); ?>
+ <?php get_footer(); ?>
---
+++ public/wp-content/themes/skinnygirl/tag.php
@@ -0,0 +1,100 @@
+<?php get_header(); ?>
+
+<link rel="stylesheet" type="text/css" href="/wp-content/themes/skinnygirl/cool-tag-cloud.css">
+
+<style>
+body.fixed-menu #head-wrapper {
+	display: block;
+}
+</style>
+
+<script>
+	$(window).on('scroll', function(e) {
+	var st = $(window).scrollTop();
+
+	var hwHeight = $('#head-wrapper').height();
+
+	if (st > hwHeight) {
+		$('body').addClass('fixed-menu');
+		
+	} else {
+		$('body').removeClass('fixed-menu');
+	}
+});
+
+</script>
+
+<div id="blog">
+
+<?php query_posts($query_string . '&cat=-9, -10, -11, -12, -13' ); ?>
+
+
+<?php if (have_posts()): while (have_posts()): the_post(); ?>
+	<div class="blogpost">
+		<?php if (has_post_thumbnail()): ?>
+			<?php the_post_thumbnail(); ?>
+		<?php endif; ?>
+        
+		<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+	    <div class='shortened'><p><?php the_excerpt(); ?></p></div>
+        <!--Share This-->
+<span class='st_facebook_large' displayText='Facebook'></span>
+<span class='st_fblike_large' displayText='Facebook Like'></span>
+<span class='st_twitter_large' displayText='Tweet'></span>
+<span class='st_pinterest_large' displayText='Pinterest'></span>
+<span class='st_email_large' displayText='Email'></span>
+<span class="orientation-flag"></span>
+<!--End Share This-->
+    </div>
+    <?php endwhile;	else:?>
+        <p><?php echo __('Sorry,no posts are available', 'whitesquare'); ?></p>
+    <?php endif; ?>
+    
+	<div class="cl"></div>
+	
+    <div id="pagination">
+		<?php echo paginate_links();?>
+	</div>
+</div>
+
+</div>
+
+<script type="text/javascript">
+	$(window).on('load', function() {
+		footer_height = $('#s4').height();
+		blog_height = $('#blog').height();
+		
+		if(!mobile) {
+			if(blog_height < 300) {
+				res = footer_height;
+				$('body .global-wrapper #s0').css('margin-bottom', '-' + parseInt(res) - (-15) + 'px');
+				$('body .global-wrapper #s0.section').css('min-height', '100% !important');
+				$('body .global-wrapper #s0.section').css('height', '100% !important');
+			} else if(blog_height <= 324) {
+				res = blog_height / 1.7;
+				$('body .global-wrapper #s0').css('margin-bottom', '-' + parseInt(res) + 'px');
+				$('body .global-wrapper #s0.section').css('min-height', '100% !important');
+				$('body .global-wrapper #s0.section').css('height', '100% !important');
+			} else {
+				if($(window).height() > 900 && blog_height < 600)
+					$('body .global-wrapper #s0.section').css('height', '90%');
+				else
+					$('body .global-wrapper #s0.section').css('height', 'auto');
+				// res = footer_height / 2;
+				// $('body .global-wrapper #s0').css('margin-bottom', '-' + parseInt(res) + (-100) + 'px');
+			}
+		} else {
+			if(blog_height < 400) {
+				res = footer_height;
+				$('body .global-wrapper #s0').css('margin-bottom', '-' + parseInt(res)-29 + 'px');
+				$('body .global-wrapper #s0.section').css('min-height', '100% !important');
+				$('body .global-wrapper #s0.section').css('height', '100% !important');
+			} else {
+				$('body .global-wrapper #s0.section').css('height', 'auto');
+			}
+		}
+	});
+</script>
+
+ <?php wp_footer(); ?>
+ <?php get_footer(); ?>

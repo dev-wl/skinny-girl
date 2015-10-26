8a45efcce4b1003954f871980a3ba15728f5ee08
wl-ak <wl-ak@wonderslab.com>
2015-10-26 11:32:02 +0200

    Release 2.0; Safari newsletter, fb feed and preloader

--- public/wp-content/themes/skinnygirl/form-subm.js
+++ public/wp-content/themes/skinnygirl/form-subm.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {	
     $('#blog form.contact-form').submit(function(e) {
      	setTimeout(function() {
-	    	if($('#blog form.contact-form input[type=submit]').val() == '✔ Subscribed') {
+     		if(navigator.userAgent.toLowerCase().indexOf('safari') > -1) {
+     			var pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i
+     			ui = $('form.contact-form input[type="email"]')[1];
+     			ui = $(ui).val();
+				if(pattern.test(ui))
+				{
+					window.stop();
+					$('p.sent-ok').css('display', 'block');
+					$('p.sent-ok').text('You have successfully subscribed');
+					setTimeout(function(){ window.location = '/'; }, 2500);
+				}
+     		}
+	    	else if($('#blog form.contact-form input[type=submit]').val() == '✔ Subscribed') {
 		    	$('p.sent-ok').css('display', 'block');
 		    	$('p.sent-ok').text('You have successfully subscribed');
 		    	setTimeout(function(){ window.location = '/'; }, 4500);

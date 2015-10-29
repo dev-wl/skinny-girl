f45b6853f3dc0557d45535566e9e5da82dcbb23e
wl-ak <wl-ak@wonderslab.com>
2015-10-29 11:06:39 +0200

    release 2.0; fixing ipad newsletter

--- public/wp-content/themes/skinnygirl/form-subm.js
+++ public/wp-content/themes/skinnygirl/form-subm.js
@@ -5,12 +5,20 @@ $(document).ready(function() {
      			var pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i
      			ui = $('form.contact-form input[type="email"]')[1];
      			ui = $(ui).val();
-				if(pattern.test(ui))
-				{
+				if(pattern.test(ui)) {
 					window.stop();
 					$('p.sent-ok').css('display', 'block');
 					$('p.sent-ok').text('You have successfully subscribed');
 					setTimeout(function(){ window.location = '/'; }, 2500);
+				}
+     		}
+     		if (navigator.userAgent.match(/iPad/i) != null) {
+     			var pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i
+     			ui = $('form.contact-form input[type="email"]')[1];
+     			ui = $(ui).val();
+				if(ui == '' || pattern.test(ui)) {
+					$('p.sent-ok').css({display: 'block', color:'red'});
+			    	$('p.sent-ok').text('Please fill in the required fields');
 				}
      		}
 	    	else if($('#blog form.contact-form input[type=submit]').val() == '✔ Subscribed') {

0d32db8e5a72590e8f648b538885cd200d486831
Patrick Jones <patrickjones@Patricks-MacBook-Pro.local>
2015-11-04 14:55:17 -0500

    Added HotJar Tracking script

--- public/wp-content/themes/skinnygirl/header.php
+++ public/wp-content/themes/skinnygirl/header.php
@@ -9,6 +9,17 @@
 		<script src="/wp-content/themes/skinnygirl/jquery.cookie.js"></script>
 
 		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"/>
+        <!-- Hotjar Tracking Code for www.skinnygirlcoffeeandteas.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:98475,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 	
 	<!--ShareThis-->
 	<script type="text/javascript">var switchTo5x=true;</script>

50611fd3fc5b35afedc61c6550b445613c1ae598
wl-ak <wl-ak@wonderslab.com>
2015-12-10 13:15:01 +0200

    Added new form handler

---
+++ public/wp-content/themes/skinnygirl/newform_subm.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {	
+    $('#blog form.contact-form').submit(function(e) {
+     	setTimeout(function() {
+     		setTimeout(function(){ window.location = '/'; }, 500);
+    	}, 800);
+    });
+});
